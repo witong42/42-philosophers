33dd76a9738fd41b2731d3cd2c3a3a53fa9e095a
williamtng <williamtong1207@gmail.com>
2024-12-07 11:33:48 +0100

    Update

---
+++ philo/srcs/checks.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 13:09:15 by witong            #+#    #+#             */
+/*   Updated: 2024/12/06 13:18:30 by witong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+bool	is_running(t_philo *philo)
+{
+	bool running;
+	pthread_mutex_lock(&philo->table->dead_lock);
+	running = philo->table->running;
+	pthread_mutex_unlock(&philo->table->dead_lock);
+	return (running);
+}
+
+void	end_simulation(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->table->dead_lock);
+	philo->table->running = false;
+	pthread_mutex_unlock(&philo->table->dead_lock);
+}
+
+void	*check_dead(void *arg)
+{
+	t_philo *philo;
+	int	i;
+
+	philo = (t_philo *)arg;
+	while (is_running(philo))
+    {
+		i = 0;
+		while (i < philo->table->philo_count)
+		{
+			pthread_mutex_lock(&philo->table->meals_lock);
+			if (realtime() - philo->table->philo[i].last_meal_time > philo->table->time_to_die)
+			{
+				pthread_mutex_unlock(&philo->table->meals_lock);
+				putstatus(DEAD, philo);
+				end_simulation(philo);
+				return (NULL) ;
+			}
+			pthread_mutex_unlock(&philo->table->meals_lock);
+			i++;
+		}
+		usleep(100);
+	}
+	return (NULL);
+}
+
+void	check_all_eaten(t_philo *philo)
+{
+	int all_full;
+	int	i;
+
+	if (philo->table->meals_required != -1)
+	{
+		pthread_mutex_lock(&philo->table->meals_lock);
+		if (philo->meals_eaten >= philo->table->meals_required && !philo->full)
+		{
+			philo->full = true;
+			all_full = 1;
+			i = 0;
+			while (i < philo->table->philo_count)
+			{
+				if (!philo->table->philo[i].full)
+				{
+					all_full = 0;
+					break ;
+				}
+				i++;
+			}
+			if (all_full)
+				end_simulation(philo);
+		}
+		pthread_mutex_unlock(&philo->table->meals_lock);
+	}
+}
---
+++ philo/srcs/init.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 16:21:53 by witong            #+#    #+#             */
+/*   Updated: 2024/12/04 15:02:19 by witong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int init_table(t_table *table)
+{
+	int	i;
+
+	table->philo = malloc(sizeof(t_philo) * table->philo_count);
+	if (!table->philo)
+		return (1);
+	i = 0;
+	while (i < table->philo_count)
+	{
+		table->philo[i].id = i + 1;
+		table->philo[i].meals_eaten = 0;
+		table->philo[i].last_meal_time = realtime();
+		table->philo[i].full = false;
+		table->philo[i].table = table;
+		i++;
+	}
+	table->running = true;
+	table->set_time = realtime();
+	table->threads = malloc(sizeof(pthread_t) * table->philo_count);
+	if (!table->threads)
+	{
+		free(table->philo);
+		return (1);
+	}
+	return (0);
+}
+
+int init_forks(t_table *table)
+{
+	int i;
+
+	table->forks = malloc(sizeof(pthread_mutex_t) * table->philo_count);
+	if (!table->forks)
+		return (cleanup(table), 1);
+	i = 0;
+	while (i < table->philo_count)
+	{
+		if (pthread_mutex_init(&(table->forks[i]), NULL) != 0)
+		{
+			while (--i >= 0)
+				pthread_mutex_destroy(&(table->forks[i]));
+			cleanup(table);
+			return (1);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < table->philo_count)
+	{
+		table->philo[i].left_fork = &table->forks[i];
+		table->philo[i].right_fork = &table->forks[(i + 1) % table->philo_count];
+		i++;
+	}
+	return (0);
+}
+
+int init(t_table *table)
+{
+	if (init_table(table) != 0)
+		return (cleanup(table), 1);
+	if (init_forks(table) != 0)
+	{
+		cleanup(table);
+		return (1);
+	}
+	if (pthread_mutex_init(&(table->write_lock), NULL) != 0)
+	{
+		cleanup(table);
+		return (1);
+	}
+	if (pthread_mutex_init(&(table->meals_lock), NULL) != 0)
+	{
+		cleanup(table);
+		return (1);
+	}
+	if (pthread_mutex_init(&(table->dead_lock), NULL) != 0)
+	{
+		cleanup(table);
+		return (1);
+	}
+	return (0);
+}
